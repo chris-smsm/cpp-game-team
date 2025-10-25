7e63810ad6ceb75b7d79f414501776f1c9d45f81
christina <yiaprosto@gmail.com>
2025-10-25 18:29:02 +0700

    добавлен файл для выбора и запуска игры

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int choice;
+
+    cout << "выберите игру: 1 -- казино" << endl;
+    cin >> choice;
+
+    switch(choice){
+        case 1: {
+
+            break;
+        }
+        case 2: {
+
+            break;
+        }
+        case 3: {
+
+            break;
+        }
+        default: {
+
+        }
+    }
+
+    return 0;
+}
